d478a9581e552ea15481b484130621d54413811c
Jimmy Kamau <jimmykambiz@gmail.com>
2019-10-08 03:50:40 +0300

    Simplify logic

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,29 @@
 // set the panic handler
 extern crate panic_semihosting;
 
-use core::sync::atomic::{AtomicBool, Ordering};
-use cortex_m::peripheral::syst::SystClkSource;
-use cortex_m_rt::{entry, exception};
+use cortex_m_rt::entry;
 use stm32f1xx_hal::prelude::*;
 use void::ResultVoidExt;
-
-static TOGGLE_LED: AtomicBool = AtomicBool::new(false);
+use embedded_hal::digital::v2::OutputPin;
 
 #[entry]
 fn main() -> ! {
-    let mut core = cortex_m::Peripherals::take().unwrap();
+    let core = cortex_m::Peripherals::take().unwrap();
     let device = stm32f1xx_hal::stm32::Peripherals::take().unwrap();
     let mut rcc = device.RCC.constrain();
     let mut flash = device.FLASH.constrain();
 
-    let clocks = rcc
-        .cfgr
-        .use_hse(8.mhz())
-        .sysclk(16.mhz())
-        .freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut delay = stm32f1xx_hal::delay::Delay::new(core.SYST, clocks);
 
     // configure the user led
     let mut gpioc = device.GPIOC.split(&mut rcc.apb2);
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
-    // configure SysTick to generate an exception every second
-    core.SYST.set_clock_source(SystClkSource::Core);
-    core.SYST.set_reload(clocks.sysclk().0);
-    core.SYST.enable_counter();
-    core.SYST.enable_interrupt();
-
     loop {
-        // sleep
-        cortex_m::asm::wfi();
-        if TOGGLE_LED.swap(false, Ordering::AcqRel) {
-            led.toggle().void_unwrap();
-        }
+        delay.delay_ms(1_000_u16);
+        led.set_high().void_unwrap();
+        delay.delay_ms(100_u16);
+        led.set_low().void_unwrap();
     }
-}
-
-#[exception]
-fn SysTick() {
-    TOGGLE_LED.store(true, Ordering::Release);
 }
